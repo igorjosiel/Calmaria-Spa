4710373072140d605fd317952bf9dae1cb50de20
Igor Josiel <igorjosielvitalino@gmail.com>
2025-04-30 21:41:19 -0300

    feat: ajusta os arquivos e adiciona interatividade Javascript no menu Dropdown.

---
+++ main.js
@@ -0,0 +1,29 @@
+function alternarSubmenu(item, mostrar) {
+  const submenu = item.querySelector(".submenu");
+
+  if (submenu) {
+    submenu.style.display = mostrar ? "block" : "none";
+
+    const menuItem = item.querySelector(".cabecalho__lista-item a");
+    menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+
+    const dropdownExpandedIcon = item.querySelector(
+      ".material-symbols-outlined.icone"
+    );
+
+    dropdownExpandedIcon.classList.toggle("active", mostrar);
+  }
+}
+
+document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
+  item.addEventListener("mouseover", () => alternarSubmenu(item, true));
+  item.addEventListener("mouseout", () => alternarSubmenu(item, false));
+  
+  item.addEventListener("click", () => {
+    const submenu = item.querySelector('.submenu');
+
+    const isDisplayed = submenu.style.display === "block";
+
+    alternarSubmenu(item, !isDisplayed);
+  });
+});
